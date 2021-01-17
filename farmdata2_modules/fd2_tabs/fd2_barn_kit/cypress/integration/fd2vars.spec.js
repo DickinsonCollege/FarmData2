050545a381cf4aee4ec2361bd6851dcef5aa6308
braughtg <braught@dickinson.edu>
2021-01-17 13:29:13 -0500

    Added id and name tags to pages in all modules

---
+++ farmdata2_modules/fd2_tabs/fd2_barn_kit/cypress/integration/fd2vars.spec.js
@@ -0,0 +1,36 @@
+describe('Check the JS vars defined by module', () => {
+
+  it('Works for admin user', () => {
+    cy.login('admin', 'farmdata2')
+
+    // Due to a Drupal issue an exception is thrown when logged in as admin.
+    // This ignores that exception and continues with the test.
+    // Note: There may be a drupal patch for this.
+    //  https://www.drupal.org/project/drupal/issues/2997194
+    cy.on('uncaught:exception', (err, runnable) => {
+      expect(err.message).to.include('parent.Drupal is undefined')
+      done()
+      return false
+    })
+
+    cy.visit('/farm/fd2-barn-kit')
+    cy.window().its('tags.fd2UserID').should('equal',1);
+    cy.window().its('tags.fd2UserName').should('equal','admin');
+  })
+
+  it('Works for manager', () => {
+    cy.login('manager1', 'farmdata2')
+
+    cy.visit('/farm/fd2-barn-kit')
+    cy.window().its('tags.fd2UserID').should('equal',3);
+    cy.window().its('tags.fd2UserName').should('equal','manager1');
+  })
+
+  it('Works for worker', () => {
+    cy.login('worker1', 'farmdata2')
+
+    cy.visit('/farm/fd2-barn-kit')
+    cy.window().its('tags.fd2UserID').should('equal',5);
+    cy.window().its('tags.fd2UserName').should('equal','worker1');
+  })
+})
