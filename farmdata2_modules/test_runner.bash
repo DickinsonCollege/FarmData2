f759cc7dd50001294acdb04fc7b1dc6a3a5147bf
braughtg <braught@dickinson.edu>
2021-06-05 09:17:50 -0400

    Docker and config for cypress component testing added

--- farmdata2_modules/test_runner.bash
+++ farmdata2_modules/test_runner.bash
@@ -2,11 +2,19 @@
 
 xhost + > /dev/null
 
+# If Dockerfile in docker/cypress is changed update
+# the version number here and below in the run command
+# to the next increment so that it will rebuild for 
+# anyone using it.
+docker build -f ../docker/cypress/Dockerfile -t cypress:fd2.1 .
+
 docker run -it \
-  -v $PWD:/$(basename $PWD) \
+  -v $PWD:/fd2test/farmdata2_modules \
+  -v $PWD/cypress:/fd2test/cypress \
+  -v $PWD/cypress.json:/fd2test/cypress.json \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
-  -w /$(basename $PWD) \
+  -w /fd2test \
   --rm \
   -e DISPLAY=$DISPLAY \
-  --entrypoint cypress \
-  cypress/included:7.4.0 open --project .
+  --entrypoint npx \
+  cypress:fd2.1 cypress open-ct --project .
