51aeebb4de962776fdb08df863fb9ad5c79ab7e1
braughtg <braught@dickinson.edu>
2021-06-05 12:23:30 -0400

    Got single file vue component testing working

---
+++ farmdata2_modules/fd2_tabs/resources/fieldsDropdownComponent.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id='fieldsDropDown'>
+        <label for='field-select'>Field: </label>
+        <select data-cy="field-select" id='field-select' @change='fieldChanged' v-model='selectedField'>
+             <option v-for='field in fieldsList'>{{field}}</option>
+        </select>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'fieldsDropdownComponent',
+    props: {
+        fieldsList: {
+            required: false,
+            default: function() { return [ 'None' ] },
+        },
+    },
+    data() {
+        return {
+            selectedField: null,
+        }
+    },
+    methods: {
+        fieldChanged: function() {
+            this.$emit('field-changed', this.selectedField)
+        },
+    },
+}
+</script>
