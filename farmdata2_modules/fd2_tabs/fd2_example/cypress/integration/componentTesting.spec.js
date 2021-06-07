b12c9682bdcefaf95b667376f1efd3a5bc20094a
josie <cookjo@dickinson.edu>
2021-06-07 09:17:44 -0400

    Commiting progress in the cypress tests of the crop and field drop-down components

--- farmdata2_modules/fd2_tabs/fd2_example/cypress/integration/componentTesting.spec.js
+++ farmdata2_modules/fd2_tabs/fd2_example/cypress/integration/componentTesting.spec.js
@@ -1,13 +1,24 @@
 describe('Field and Crop Dropdowns', () => {
   beforeEach(() => {
-          //cy.login('manager1', 'farmdata2')
-          cy.visit('/farm/fd2-example/componentTesting')
+        cy.login('manager1', 'farmdata2')
+        cy.visit('/farm/fd2-example/cropAndFieldPickersTesting')
   })
   context('Crop Dropdown', () => {
     it('accepts input from search bar', () => {
-      cy.get('crop-picker-element')
-        .type('lettuce')
-        .should('have.value', 'lettuce')
+        cy.get('[data-cy=crop-picker-input]')
+            .type('lettuce')
+            .should('have.value', 'lettuce')
+    })
+
+    //create test to make sure that all crops are showing up in the drop-down menu before anything is typed in the search bar
+    //API request in the spec file???
+
+    it('displays only crops sorted out by the search term', () => {
+        cy.get('[data-cy=crop-picker-input]')
+            .type('lettuce')
+            .should('have.value', 'lettuce')
+        cy.get('[data-cy=crop-picker-dropdown]')
+            .should('have.value', ['placeholder'])
     })
   })
 })
