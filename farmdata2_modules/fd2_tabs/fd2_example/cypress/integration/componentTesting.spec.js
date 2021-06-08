446b81f6dc51867e4f50efaa9157dd614c6f0994
josie <cookjo@dickinson.edu>
2021-06-08 09:32:28 -0400

    Crop and Field Picker component and cypress tests in progress

--- farmdata2_modules/fd2_tabs/fd2_example/cypress/integration/componentTesting.spec.js
+++ farmdata2_modules/fd2_tabs/fd2_example/cypress/integration/componentTesting.spec.js
@@ -1,24 +1,62 @@
 describe('Field and Crop Dropdowns', () => {
-  beforeEach(() => {
+    beforeEach(() => {
         cy.login('manager1', 'farmdata2')
         cy.visit('/farm/fd2-example/cropAndFieldPickersTesting')
-  })
-  context('Crop Dropdown', () => {
-    it('accepts input from search bar', () => {
-        cy.get('[data-cy=crop-picker-input]')
-            .type('lettuce')
-            .should('have.value', 'lettuce')
+        
     })
+    context('Crop Dropdown', () => {
+        it('accepts input from search bar', () => {
+            cy.get('[data-cy=crop-picker-input]')
+                .type('Beans')
+                .should('have.value', 'Beans')
+        })
 
-    //create test to make sure that all crops are showing up in the drop-down menu before anything is typed in the search bar
-    //API request in the spec file???
+        it.only('contains the right crops', () => {
+            cy.get('[data-cy=crop-picker-options]')
+                .first().should('have.text', 'All')
+                .then().should('have.text', 'Corn')
+                .then().should('have.text', 'Beans')
+                .then().should('have.text', 'Peas')
+        })
 
-    it('displays only crops sorted out by the search term', () => {
-        cy.get('[data-cy=crop-picker-input]')
-            .type('lettuce')
-            .should('have.value', 'lettuce')
-        cy.get('[data-cy=crop-picker-dropdown]')
-            .should('have.value', ['placeholder'])
+        it('includes the all option when the attribute is included', () => {
+            //test that it has the attribute 'includesAll'
+            cy.get('[data-cy=crop-picker-options]')
+                .first().should('have.text', 'All')
+        })
+
+        /**
+         *         it('populates search bar when an option is selected', () => {
+            cy.get('[data-cy=crop-picker-input]')
+                .type('Beans')
+            cy.get('[data-cy=crop-picker-options]')
+                .first().should('have.text', 'Beans')
+                .next().should('have.text', 'Soybeans')
+        })
+         */
+
+        /**
+         * it.only('updates the selectedCrop when an option is selected', () => {
+            cy.get('[data-cy=crop-picker-input]')
+                .type('Beans')
+                .should('have.value', 'Beans')
+            cy.get('[data-cy=crop-picker-component]')
+                .should('have.data', 'selectedCrop')
+        })
+         */
+        
     })
-  })
+    context('Field Dropdown', () => {
+        it('accepts input from search bar', () => {
+            cy.get('[data-cy=field-picker-input]')
+                .type('Jasmine-1')
+                .should('have.value', 'Jasmine-1')
+        })
+
+        it('includes "all" option when the attribute is included', () => {
+            //test that it has the attribute 'includesAll'
+            cy.get('[data-cy=crop-picker-options]')
+                .first().should('have.text', 'All')
+        })
+    })
 })
