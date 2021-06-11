b3f5b69a0442fd2e09538e8dd15b49629f40fb1c
josieecook <cookjo@dickinson.edu>
2021-06-11 10:28:41 -0400

    Updated cypress tests to be more efficient in their use of mounts

--- farmdata2_modules/fd2_tabs/resources/dropdownWithAllComponent.spec.comp.js
+++ farmdata2_modules/fd2_tabs/resources/dropdownWithAllComponent.spec.comp.js
@@ -4,60 +4,53 @@ var DropdownComp = require("./dropdownWithAllComponent.js")
 var DropdownWithAllComponent = DropdownComp.DropdownWithAllComponent
 
 describe('Field and Crop Dropdowns', () => {
-    it('renders the dropdown menu', () => {
-        mount(DropdownWithAllComponent, {
-            propsData: {
-                dropdownList: [ 'Beans', 'Corn', 'Peas'],
-            }
+    context('without includesAll property', () => {
+        beforeEach(() => {
+            mount(DropdownWithAllComponent, {
+                propsData: {
+                    dropdownList: [ 'Beans', 'Corn', 'Peas'],
+                }
+            })
         })
-        cy.get('[data-cy=dropdown-component]').should('exist')
-    })
+        
+        it('renders the dropdown menu', () => {
+            cy.get('[data-cy=dropdown-component]').should('exist')
+        })
+        
+        it('accepts input from search bar', () => {
+            cy.get('[data-cy=dropdown-input]')
+                .type('Beans')
+                .should('have.value', 'Beans')
+        })
     
-    it('accepts input from search bar', () => {
-        mount(DropdownWithAllComponent, {
-            propsData: {
-                dropdownList: [ 'Beans', 'Corn', 'Peas'],
-            }
+        it('contains the right crops and excludes all', () => {
+            cy.get('[data-cy=singleOption]')
+                .first().should('have.text', 'Beans')
+                .next().should('have.text', 'Corn')
+                .next().should('have.text', 'Peas')
         })
-
-        cy.get('[data-cy=dropdown-input]')
-            .type('Beans')
-            .should('have.value', 'Beans')
-    })
-  
-    it('contains the right crops and excludes all', () => {
-        mount(DropdownWithAllComponent, {
-            propsData: {
-                dropdownList: ['Corn', 'Beans', 'Peas'],
-            }
-        })
-        cy.get('[data-cy=singleOption]')
-            .first().should('have.text', 'Corn')
-            .next().should('have.text', 'Beans')
-            .next().should('have.text', 'Peas')
     })
 
-    it('includes the all option when the attribute is included', () => {
-        mount(DropdownWithAllComponent, {
-            propsData: {
-                dropdownList: ['Corn', 'Beans', 'Peas'],
-                includesAll: true
-            }
+    context('with includesAll property', () => {
+        beforeEach(() => {
+            mount(DropdownWithAllComponent, {
+                    propsData: {
+                        dropdownList: ['Corn', 'Beans', 'Peas'],
+                        includesAll: true
+                    }
+            })
         })
-        cy.get('[data-cy=singleOption]')
-            .first().should('have.text', 'All')
-    })
 
-    it('will not accept inputs that are not in the menu', () => {
-        mount(DropdownWithAllComponent, {
-            propsData: {
-                dropdownList: ['Corn', 'Beans', 'Peas'],
-                includesAll: true
-            }
+        it('includes the all option when the attribute is included', () => {
+            cy.get('[data-cy=singleOption]')
+                .first().should('have.text', 'All')
         })
-        cy.get('[data-cy=dropdown-input]')
-            .type('Lettuce')
-            .blur()
-            .should('have.value', '')
+
+        it('will not accept inputs that are not in the menu', () => {
+            cy.get('[data-cy=dropdown-input]')
+                .type('Lettuce')
+                .blur()
+                .should('have.value', '')
+        })
     })
 })
