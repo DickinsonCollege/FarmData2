3b29deb550a76fbb3ee323e97fb032f3b1a89f08
josieecook <cookjo@dickinson.edu>
2021-06-18 10:19:46 -0400

    in progress api request function

--- farmdata2_modules/fd2_tabs/resources/APIRequestFunction.js
+++ farmdata2_modules/fd2_tabs/resources/APIRequestFunction.js
@@ -5,85 +5,43 @@ catch {
     axios = require('axios')
 }
 
-/*
-function recursiveAPICall(url) {
-    console.log('start of recursiveAPICall')
-    let requestURL = url
-    let response = null
-
-    console.log('before api call')
-    response = axios.get(requestURL)
-        console.log('after api call')
-        next = response.data.next  
-        console.log(next)            
-        if (typeof next == 'undefined') {
-            console.log('in the if')
-            return response
-        }
-        else {
-            console.log('in the else')
-            fullRequestURL = response.data.next
-            requestURL = fullRequestURL.substring(16, fullRequestURL.length);
-
-            fullResponse = [].concat(response, this.apiRequest(requestURL));
-
-            console.log(response)
-            return fullResponse
-        }
-
-} */
-
-function apiCall(url) {
-    let requestURL = url
-    let response = null
-
-    response = axios.get(requestURL)
-
-    next = response.data
-    
-    return response
-}
-
-function apiRequest(url) {
+function helperAPIRequest(url) {
     return new Promise ((resolve, reject) => {
-        response = this.apiCall(url)
+        let requestURL = url
+        let response = null
 
+        response = axios.get(requestURL)
+
+        console.log(response)
         if (response != null) {
-            resolve(response)
+            console.log(response)
+            return resolve(response)
         }
         else {
-            reject('API request failed')
+            return reject('API request failed')
         }
     })
 }
+function apiRequest(url) {
+    return new Promise((resolve, reject) => {
+        let next = null
+        helperAPIRequest(url).then((response) => {
+            console.log("finished calling helper method")
+                next = response.data.next
+                console.log(next)
+            }).catch((failed) => {
+                console.log('In the Catch')
+            })
 
-
-
-/*
-function apiRequest(url, responseArray) {
-            let requestURL = url
-            responseArray = [1]
-            //let responseArray = []
-
-                axios.get(requestURL)
-                    .then(response => {
-                        next = response.data.next
-                        
-                        if (typeof next == 'undefined') {
-                            return response.data.list
-                        }
-                        else {
-                            fullRequestURL = response.data.next
-                            requestURL = fullRequestURL.substring(16, fullRequestURL.length);
-
-                            responseArray = response.data.list
-
-                            responseArray = [].concat(responseArray, this.apiRequest(requestURL));
-                        }
-                    })
-                return responseArray
+        if (next != null) {
+            console.log(next)
+            resolve('got next')
+        }
+        else {
+            reject("no next")
+        }
+    })
 }
-*/
 
 try {
     module.exports = {
