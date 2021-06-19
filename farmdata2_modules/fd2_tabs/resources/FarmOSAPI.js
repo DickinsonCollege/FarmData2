1873b27d17ca35db66d6b034ed4131eb3a5dd884
braughtg <braught@dickinson.edu>
2021-06-19 15:27:14 -0400

    Working version of getAllPages along with some testng.
    @Co-authored-by: josiecook <cookjo@dickinson.edu>

--- farmdata2_modules/fd2_tabs/resources/FarmOSAPI.js
+++ farmdata2_modules/fd2_tabs/resources/FarmOSAPI.js
@@ -10,17 +10,22 @@ catch {
 // updates apply to all pages.
 
 function getAllPages(url, arr) {
-    // Concatenates the contents of the list property from 
-    // all pages of a multipage response into arr
+    // Retrieves all pages of a multipage response.
+    // Usage:   
+    //    let result = []
+    //    getAllPages(url, result)
+    //
+    // The result array will be filled with the elements from
+    // response.data.list from all of the pages.  Each page of
+    // responses is added to the array as it is retrieved.
+    
     return new Promise((resolve, reject) => {
-        
         axios.get(url)
         .then(function(response) {
-            return JSON.parse(response.data)
+            return response.data
         })
         .then(function(data) {
             arr.push.apply(arr,data.list)
-
             if (!data.hasOwnProperty('next')) {
                 resolve()
             }
