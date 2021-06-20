3516a82f580068279ab54912a9bc478e87a3f2ab
braughtg <braught@dickinson.edu>
2021-06-20 13:30:12 -0400

    Added working cypress tests. Now tests for a single page request and for a multiple page request.
    Co-authored-by: josiecook <cookjo@dickinson.edu>
    Co-authored-by: IrisSC <shakerci@dickinson.edu>

--- farmdata2_modules/fd2_tabs/resources/FarmOSAPI.spec.js
+++ farmdata2_modules/fd2_tabs/resources/FarmOSAPI.spec.js
@@ -9,29 +9,42 @@ describe('API Request Function', () => {
         testArray = []
     })
 
-    /*
-    it.only('Test on a request with one page.', () => {
-        // Note: For some reason the following does not work. It should
-        // intercept all requests to log but it does not match any.
-        // Maybe a cypress bug?  Maybe a future version will fix it?
+    it('Test on a request with one page.', () => {
+        cy.intercept("GET",/log\?type=farm_seeding/).as('onlyone')
 
-        cy.intercept('GET','/log?').as('onlyone')
-        getAllPages('/log?type=farm_seeding&id[lt]=1500', testArray)
+        let calls=0
 
-        cy.wait('@onlyone').should(() => {
-            cy.wrap(testArray).should('have.length.equal',100)
+        // This wait is a bit of a hack.  
+        // Without it the first request is not intercepted.
+        // There may be a more correct way to wait?
+        cy.wait(1)
+        .then(() => {
+            cy.wait('@onlyone').should(() => {
+                cy.wrap(testArray).should('have.length.gt',0)
+                calls++
+            })
+
+            getAllPages('/log?type=farm_seeding&id[lt]=1500', testArray)
+        })
+        .then(() => {
+            expect(calls).to.equal(1)
         })
     })
-    */
 
     it('Test on a request with multiple pages', () => {
-        cy.intercept("GET","/log?type=farm_seeding").as('first')
+        cy.intercept("GET",/log\?type=farm_seeding$/).as('first')
         cy.intercept("GET","/log?type=farm_seeding&page=1").as('second')
         cy.intercept("GET","/log?type=farm_seeding&page=2").as('third')
+     
+        let firstCalls=0
+        let secondCalls=0
+        let thirdCalls=0
 
-        cy.wait(1).then(function() {
+        cy.wait(1)
+        .then(() => {
             cy.wait('@first').should(() => {
                 cy.wrap(testArray).should('have.length.gt',0)
+                firstCalls++
             })
 
             cy.wait('@second').should(() => {
@@ -41,13 +54,21 @@ describe('API Request Function', () => {
                 // Note: depending on timing this may not run until after any
                 // subsequent calls.
                 cy.wrap(testArray).should('have.length.gt',100)
+                secondCalls++
             })
 
             cy.wait('@third').should(() => {
                 cy.wrap(testArray).should('have.length.gt',200)
+                thirdCalls++
             })
 
             getAllPages("/log?type=farm_seeding", testArray)
+
+        })
+        .then(() => {
+            expect(firstCalls).to.equal(1)
+            expect(secondCalls).to.equal(1)
+            expect(thirdCalls).to.equal(1)
         })
     })
 })
