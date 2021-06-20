286e52eb8f435cc195c6e2565d6a83cbd7c82a90
Grant Braught <braught@dickinson.edu>
2021-06-20 13:35:33 -0400

    Refines and Completes getAllPages (#195)
    * Cypress test now working.
    Not sure the wait(1) is the right way to do it.
    Will continue to revisit.
    
    * Added working cypress tests.
    Now tests for a single page request and for a multiple page
    request.
    
    Co-authored-by: josiecook <cookjo@dickinson.edu>
    Co-authored-by: IrisSC <shakerci@dickinson.edu>

--- farmdata2_modules/fd2_tabs/resources/FarmOSAPI.spec.js
+++ farmdata2_modules/fd2_tabs/resources/FarmOSAPI.spec.js
@@ -9,43 +9,66 @@ describe('API Request Function', () => {
         testArray = []
     })
 
-    /*
-    it.only('Test on a request with one page.', () => {
-        // Note: For some reason the following does not work. It should
-        // intercept all requests to log but it does not match any.
-        // Maybe a cypress bug?  Maybe a future version will fix it?
+    it('Test on a request with one page.', () => {
+        cy.intercept("GET",/log\?type=farm_seeding/).as('onlyone')
 
-        cy.intercept('GET','/log?').as('onlyone')
-        getAllPages('/log?type=farm_seeding&id[lt]=1500', testArray)
+        let calls=0
 
-        cy.wait('@onlyone').should(() => {
-            cy.wrap(testArray).should('have.length.equal',100)
+        // This wait is a bit of a hack.  
+        // Without it the first request is not intercepted.
+        // There may be a more correct way to wait?
+        cy.wait(1)
+        .then(() => {
+            cy.wait('@onlyone').should(() => {
+                cy.wrap(testArray).should('have.length.gt',0)
+                calls++
+            })
+
+            getAllPages('/log?type=farm_seeding&id[lt]=1500', testArray)
+        })
+        .then(() => {
+            expect(calls).to.equal(1)
         })
     })
-    */
 
     it('Test on a request with multiple pages', () => {
-        // Note: For some reasson the following did not work to intecept
-        // the original request.
-        // cy.intercept('GET','/log?type=farm_seeding').as('original')
-        // Maybe a cypress bug?  Maybe a future version will fix it?
+        cy.intercept("GET",/log\?type=farm_seeding$/).as('first')
+        cy.intercept("GET","/log?type=farm_seeding&page=1").as('second')
+        cy.intercept("GET","/log?type=farm_seeding&page=2").as('third')
+     
+        let firstCalls=0
+        let secondCalls=0
+        let thirdCalls=0
 
-        cy.intercept('GET','/log?type=farm_seeding&page=1').as('second')
-        cy.intercept('GET','/log?type=farm_seeding&page=2').as('third')
-        
-        getAllPages('/log?type=farm_seeding', testArray)
+        cy.wait(1)
+        .then(() => {
+            cy.wait('@first').should(() => {
+                cy.wrap(testArray).should('have.length.gt',0)
+                firstCalls++
+            })
 
-        cy.wait('@second').should(() => {
-            // Just by getting here we know the second page was requested.
+            cy.wait('@second').should(() => {
+                // Just by getting here we know the second page was requested.
 
-            // Check that data made it into testArray.
-            // Note: depending on timing this may not run until after any
-            // subsequent calls.
-            cy.wrap(testArray).should('have.length.gt',100)
+                // Check that data made it into testArray.
+                // Note: depending on timing this may not run until after any
+                // subsequent calls.
+                cy.wrap(testArray).should('have.length.gt',100)
+                secondCalls++
+            })
+
+            cy.wait('@third').should(() => {
+                cy.wrap(testArray).should('have.length.gt',200)
+                thirdCalls++
+            })
+
+            getAllPages("/log?type=farm_seeding", testArray)
+
         })
-
-        cy.wait('@third').should(() => {
-            cy.wrap(testArray).should('have.length.gt',200)
+        .then(() => {
+            expect(firstCalls).to.equal(1)
+            expect(secondCalls).to.equal(1)
+            expect(thirdCalls).to.equal(1)
         })
     })
 })
