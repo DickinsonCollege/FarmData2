db7c6cde3863f79ec35473ebee76a1801248859f
braughtg <braught@dickinson.edu>
2021-06-28 18:09:58 -0400

    Added cropID to data in tray seedings

--- docker/sampleDB/addTraySeedings.py
+++ docker/sampleDB/addTraySeedings.py
@@ -211,7 +211,10 @@ def addSeeding(row, plantingID, seedingTypeID):
             "id": userMap[row[1]],
             "resource": "user"
         }],
-        "lot_number": row[7]
+        "lot_number": row[7],
+        "data": json.dumps({ 
+            "cropID": cropMap[row[3]] 
+        })
     }
 
     return addLog(seeding)
