d5c11c429b2571279b1ab3b1be4e1cdcc126bfba
josieecook <60167626+josieecook@users.noreply.github.com>
2021-07-07 10:41:56 -0400

    Table Component - Optional visibleColumns (#230)
    * Made the visibleColumns prop in the table component optional, if it is not specified then all columns are visible.
    
    Co-authored-by: IrisSC <shakerci@dickinson.edu>

--- farmdata2_modules/fd2_tabs/resources/CustomTableComponent.js
+++ farmdata2_modules/fd2_tabs/resources/CustomTableComponent.js
@@ -3,14 +3,14 @@ let CustomTableComponent = {
                     <table data-cy="custom-table" style="width:100%" class="table table-bordered table-striped">
                         <thead>
                             <tr class="sticky-header">
-                                <th v-if="visibleColumns[index]" data-cy="headers" v-for="(header, index) in headers">{{ header }}</th>
+                                <th v-if="isVisible[index]" data-cy="headers" v-for="(header, index) in headers">{{ header }}</th>
                                 <th data-cy="edit-header" width=55 v-if="canEdit">Edit</th>
                                 <th data-cy="delete-header" width=55 v-if="canDelete">Delete</th>
                             </tr>
                         </thead>
                         <tbody>
                             <tr data-cy="object-test" v-for="(row, index) in rows">
-                                <td v-if="visibleColumns[itemIndex]" data-cy="table-data" v-for="(item, itemIndex) in row.data">
+                                <td v-if="isVisible[itemIndex]" data-cy="table-data" v-for="(item, itemIndex) in row.data">
                                     <div v-if="!(rowToEdit==index) || inputOptions[itemIndex].type == 'no input'" v-html="item"></div>
                                     
                                     <textarea data-cy="test-input" v-if="rowToEdit==index && inputOptions[itemIndex].type == 'text'" v-model="row.data[itemIndex]" @focusout="changedCell(itemIndex)"></textarea>
@@ -53,7 +53,7 @@ let CustomTableComponent = {
         },
         visibleColumns: {
             type: Array,
-            required: true
+            default: null
         },
         inputOptions: {
             type: Array,
@@ -63,7 +63,8 @@ let CustomTableComponent = {
     data() {
         return {
             rowToEdit: null,
-            indexesToChange: []
+            indexesToChange: [],
+            isVisible: []
         }
     },
     methods: {
@@ -99,6 +100,17 @@ let CustomTableComponent = {
             }
         },
     },
+    created() {
+        if (this.visibleColumns == null) {
+            for (i = 0; i < this.headers.length; i++) {
+                this.isVisible.push(true);
+            }
+        }
+        else {
+            this.isVisible = this.visibleColumns
+        }
+        
+    }
 }
 
 try {
