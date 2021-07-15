f9735b6eab0a960c0f4dc3756b3c9f4f1df00312
IrisSC <shakerci@dickinson.edu>
2021-07-15 13:01:26 -0500

    Added test for slecting an area and inputting a number of workers.

--- farmdata2_modules/fd2_tabs/fd2_field_kit/seedingInput.spec.js
+++ farmdata2_modules/fd2_tabs/fd2_field_kit/seedingInput.spec.js
@@ -4,9 +4,10 @@ describe('Test the seeding input page', () => {
     beforeEach(() => {
         cy.login('manager1', 'farmdata2')
     })
-    context.only('sets up the page', () => {
+    context('sets up the page', () => {
         it('visits the page', () => {
             cy.visit('/farm/fd2-field-kit/seedingInput')
+            cy.wait(40000)
         })
     })
     context('test inputs and buttons', () => {
@@ -23,16 +24,35 @@ describe('Test the seeding input page', () => {
                 .should('exist')
                 .type('2021-05-07')
         })
-        it.only('select a crop', () => {
+        it('select a crop', () => {
             cy.get('[data-cy=crop-selection')
                 .should('exist')
             
             cy.get('[data-cy=dropdown-input').then(($dropdowns) => {
                 cy.get($dropdowns[0]).should('exist')
-                .select('BEAN')
-                .should('have.value', 'BEAN')
-            })
-                
+                    .select('BEAN')
+                    .should('have.value', 'BEAN')
+            })  
         })
+        it('input num of workers', () => {
+            cy.get('[data-cy=num-workers]')
+                .should('exist')
+                .click()
+                .clear()
+                .type('2')
+                .should('have.value', '2')
+        })
+        //last b/c it take sthe longest to load in
+        it('select a area', () => {
+            cy.get('[data-cy=area-selection')
+                .should('exist') 
+            
+            cy.get('[data-cy=dropdown-input]').then(($dropdowns) => {
+                cy.get($dropdowns[1]).should('exist')
+                    .select('A')
+                    .should('have.value', 'A')
+            })
+        })
+        
     })
 })
