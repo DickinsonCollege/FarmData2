62629f01ed4b79cdc10d167ac6df0a932c2ac8f6
IrisSC <shakerci@dickinson.edu>
2021-07-15 11:47:57 -0500

    now tests that the submit button is disabled when the page loads, that the user can put in a new date, and that the user can select a crop.

--- farmdata2_modules/fd2_tabs/fd2_field_kit/seedingInput.spec.js
+++ farmdata2_modules/fd2_tabs/fd2_field_kit/seedingInput.spec.js
@@ -4,4 +4,35 @@ describe('Test the seeding input page', () => {
     beforeEach(() => {
         cy.login('manager1', 'farmdata2')
     })
+    context.only('sets up the page', () => {
+        it('visits the page', () => {
+            cy.visit('/farm/fd2-field-kit/seedingInput')
+        })
+    })
+    context('test inputs and buttons', () => {
+        it('button is disabled', () => {
+            cy.get('[data-cy=submit-button]')
+                .should('exist')
+                .should('be.disabled')
+        })
+        it('input new date', () => {
+            cy.get('[data-cy=date-selection')
+                .should('exist')
+
+            cy.get('[data-cy=date-select')
+                .should('exist')
+                .type('2021-05-07')
+        })
+        it.only('select a crop', () => {
+            cy.get('[data-cy=crop-selection')
+                .should('exist')
+            
+            cy.get('[data-cy=dropdown-input').then(($dropdowns) => {
+                cy.get($dropdowns[0]).should('exist')
+                .select('BEAN')
+                .should('have.value', 'BEAN')
+            })
+                
+        })
+    })
 })
