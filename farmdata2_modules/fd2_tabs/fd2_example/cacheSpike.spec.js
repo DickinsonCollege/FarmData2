7ffe3bc390e79fcf2467fdb51ec6de8ee93d0bdc
IrisSC <shakerci@dickinson.edu>
2021-10-14 12:51:33 -0400

    uncommented reload tests.

--- farmdata2_modules/fd2_tabs/fd2_example/cacheSpike.spec.js
+++ farmdata2_modules/fd2_tabs/fd2_example/cacheSpike.spec.js
@@ -2,26 +2,19 @@ describe('testing the cache spike page', () => {
     beforeEach(() => {
         cy.login('manager1', 'farmdata2', {timeout: 60000})
 
-        //cy.visit('/farm/fd2-example/cacheSpike', {timeout: 120000})
+        cy.visit('/farm/fd2-example/cacheSpike', {timeout: 120000})
     })
     it.only('test that it saves what is written in input', () => {
-        /*cy.get('[data-cy=text-input]')
+        cy.get('[data-cy=text-input]')
             .should('exist')
             .type('Saving to Cache')
-            .should('have.value', 'Saving to Cache')*/
-
-        cy.visit('/farm/fd2-example/cacheSpike', {timeout: 120000})
-            .then(() => {
-                cy.getCookie('testKey').should('exist')
-            })
-
-        
-
-        /*cy.reload()
+            .should('have.value', 'Saving to Cache')
+            
+        cy.reload()
         cy.wait(1000)
 
         cy.get('[data-cy=text-input]')
-            .should('have.value', 'Saving to Cache')*/
+            .should('have.value', 'Saving to Cache')
     })
     it('test that it saves array in cache', () => {
         
