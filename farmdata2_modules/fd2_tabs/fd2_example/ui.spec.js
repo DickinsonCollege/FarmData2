4693d68f17b661348b45a8ce2d50815f9a8a1b6e
braughtg <braught@dickinson.edu>
2021-12-22 15:14:38 -0500

    Added test for spinner

--- farmdata2_modules/fd2_tabs/fd2_example/ui.spec.js
+++ farmdata2_modules/fd2_tabs/fd2_example/ui.spec.js
@@ -169,4 +169,31 @@ describe('Test the UI component', () => {
                 .should('have.text', '12')
         })
     })
+
+    context('check the spinner example', () => {
+
+        it('spinner appears while loading', () => {
+            cy.get('[data-cy=loading-spinner]')
+                .should('not.exist')
+
+            cy.get('[data-cy=fetch-seeding-logs]')
+                .click()
+
+            cy.get('[data-cy=loading-spinner]')
+                .should('exist')
+            cy.get('[data-cy=first-log-name]')
+                .should('have.text','2019-02-04 RADISH CHUAU-2')
+            cy.get('[data-cy=loading-spinner]')
+                .should('exist')
+            cy.get('[data-cy=last-log-name]')
+                .should('have.text','2019-03-25 PEPPERS-BELL')
+            cy.get('[data-cy=loading-spinner]')
+                .should('exist')
+            cy.get('[data-cy=last-log-name]')
+                .should('have.text','2019-04-26 LEEK')
+                
+            cy.get('[data-cy=loading-spinner]')
+                .should('not.exist')
+        })
+    })
 })
