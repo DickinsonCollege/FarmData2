eed7b885a68803de88687148fbd4ad36ab9402cc
braughtg <braught@dickinson.edu>
2021-12-31 13:30:20 -0500

    Added JSDoc comments

--- farmdata2_modules/fd2_tabs/resources/DropdownWithAllComponent.js
+++ farmdata2_modules/fd2_tabs/resources/DropdownWithAllComponent.js
@@ -1,5 +1,11 @@
 /**
- * Vue.js component that defines the dropdown menu.
+ * A dropdown menu where "All" can be included as the first option.
+ * 
+ * @vue-prop {Array} dropDownList - The contents of the dropdown. Changes to an array bound to this prop will appear in the list.
+ * @vue-prop {Boolean} [includesAll=false] - true to include the All option at the top of the list of options. All will be omitted if includesAll is false or omitted.
+ * @vue-prop {String} [defaultInput] - the option to be selected in the component. Changes to a value bound to this prop will change the selected option.
+ *
+ * @vue-event {String} selection-changed - Emits the selected option when the option is changed. Note: This event is also emitted in the mounted lifecycle hook to signal the setting of the initial value.
  */ 
 let DropdownWithAllComponent = {
     template: `<div data-cy="dropdown-component">
