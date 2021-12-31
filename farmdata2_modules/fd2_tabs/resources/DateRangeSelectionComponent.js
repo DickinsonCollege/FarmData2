b9f4851b280810a7b1be19a2b10b92d1c0bc024e
braughtg <braught@dickinson.edu>
2021-12-31 14:03:46 -0500

    Added JSDoc for component

--- farmdata2_modules/fd2_tabs/resources/DateRangeSelectionComponent.js
+++ farmdata2_modules/fd2_tabs/resources/DateRangeSelectionComponent.js
@@ -6,6 +6,16 @@ catch(err) {
     DateSelectionComponent = DateComps.DateSelectionComponent
 }
 
+/**
+ * A Vue component for selecting a range of dated (i.e. start and end dates).  The component ensures that the user cannot choose a start date after the end date and vice versa.
+ * 
+ * @vue-prop {String} defaultStartDate - the initial start date to be displayed (YYYY-MM-DD).
+ * @vue-prop {String} defaultEndDate - the initial end date to be displayed (YYYY-MM-DD).
+ * 
+ * @vue-event click - Emits an event with no payload when when one of the date selection elements is clicked.  This event does not necessarily indicate a change in date.
+ * @vue-event {String} start-date-changed - Emits the selected date (YYYY-MM-DD) when a start date is entered or chosen and the start date selection element loses focus.
+ * @vue-event {String} end-date-changed - Emits the selected date (YYYY-MM-DD) when an end date is entered or chosen and the end date selection element loses focus.
+ */
 let DateRangeSelectionComponent = {
     template: `<div>
                 <date-selection data-cy="start-date-select" :defaultDate="defaultStartDate" :latestDate="latestStartDate" @date-changed="startDateChange" 
