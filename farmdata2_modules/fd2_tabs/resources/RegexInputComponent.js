a9ced3b47643a6353f41426bb8fd56e295c97f4c
FutzMonitor <cgonzalez.smdc@gmail.com>
2022-06-16 10:54:54 -0400

    Added some lines of code from the reference Vue component linked in the original issue. Additionally added some lines in Seeding Input to begin testing this functionality. (Changes are being committed to work on another branch).

--- farmdata2_modules/fd2_tabs/resources/RegexInputComponent.js
+++ farmdata2_modules/fd2_tabs/resources/RegexInputComponent.js
@@ -4,7 +4,57 @@ let RegexInputComponent = {
         <input style='height: 25px; width: 4em;' type='number'>
          </span>`, 
 
-}
+         props: {
+            modelValue: {
+              required: true,
+              type: [Number, String]
+            },
+            // Using for: String.prototype.replace(regexp, replacement)
+            regExp: {
+              type: RegExp,
+              default: null
+            },
+            // Using for: String.prototype.replace(regexp, replacement)
+            replacement: {
+              type: String,
+              default: ''
+            }
+          },
+          //emits: ['update:modelValue'],
+          data () {
+            return {
+              val: ''
+            }
+          },
+          methods: {
+            // format the value of input
+            formatValue (val) {
+              const formattedValue = val.toString().replace(this.regExp, this.replacement)
+              return formattedValue
+            },
+            // update the value of input
+            updateValue (val) {
+              const formattedValue = this.formatValue(val)
+              this.val = formattedValue
+              this.emitInput(formattedValue)
+            },
+            // emit input event
+            emitInput (val) {
+              this.$emit('update:modelValue', val)
+            }
+          },
+          watch: {
+            // watch value prop
+            modelValue: {
+              handler (val = '') {
+                if (val !== this.val) {
+                  this.updateValue(val)
+                }
+              },
+              immediate: true
+            }
+          }
+        }
 
 
 /*
