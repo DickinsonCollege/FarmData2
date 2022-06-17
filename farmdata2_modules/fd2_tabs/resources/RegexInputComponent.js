0e45237909dae7a41f5e987386700a13d0425306
FutzMonitor <cgonzalez.smdc@gmail.com>
2022-06-17 11:58:58 -0400

    Putting these away in order to switch to another branch

--- farmdata2_modules/fd2_tabs/resources/RegexInputComponent.js
+++ farmdata2_modules/fd2_tabs/resources/RegexInputComponent.js
@@ -1,60 +1,68 @@
 let RegexInputComponent = {
     template: 
         `<span>
-        <input v-model.number="val" @change='formatValue(val)' style='height: 25px; width: 4em;' type='number'>
+        <input @input="updateValue($event.target.value)"
+        @change="emitChange"
+        v-model="val" style='height: 25px; width: 4em;' type='number'>
          </span>`, 
 
          props: {
-            modelValue: {
+          value: {
               required: true,
               type: [Number, String]
-            },
-            // Using for: String.prototype.replace(regexp, replacement)
-            regExp: {
+          },
+          // Using for: String.prototype.replace(regexp, replacement)
+          regExp: {
               type: RegExp,
               default: null
-            },
-            // Using for: String.prototype.replace(regexp, replacement)
-            replacement: {
+          },
+          // Using for: String.prototype.replace(regexp, replacement)
+          replacement: {
               type: String,
               default: ''
-            }
-          },
-          //emits: ['update:modelValue'],
-          data () {
-            return {
-              val: ''
-            }
-          },
-          methods: {
-            // format the value of input
-            formatValue (val) {
-              const formattedValue = val.toString().replace(this.regExp, this.replacement)
-              return formattedValue
-            },
-            // update the value of input
-            updateValue (val) {
-              const formattedValue = this.formatValue(val)
-              this.val = formattedValue
-              this.emitInput(formattedValue)
-            },
-            // emit input event
-            emitInput (val) {
-              this.$emit('update:modelValue', val)
-            }
-          },
-          watch: {
-            // watch value prop
-            modelValue: {
-              handler (val = '') {
-                if (val !== this.val) {
-                  this.updateValue(val)
-                }
-              },
-              immediate: true
-            }
           }
-        }
+      },
+      data() {
+          return {
+              val: ''
+          };
+      },
+      methods: {
+          // format the value of input
+          formatValue(val) {
+              const formattedValue = val.toString().replace(this.regExp, this.replacement);
+
+              return formattedValue;
+          },
+
+          // update the value of input
+          updateValue(val) {
+              const formattedValue = this.formatValue(val);
+
+              this.val = formattedValue;
+              this.emitInput(formattedValue);
+          },
+
+          // emit input event
+          emitInput(val) {
+              this.$emit('input', val);
+          },
+
+          // emit change event
+          emitChange() {
+              this.$emit('change', this.val);
+          }
+      },
+      watch: {
+          // watch value prop
+          value(val) {
+              if (val !== this.val) {
+                  this.updateValue(val)
+              }
+          }
+      }
+  }
+        
 
 
 /*
