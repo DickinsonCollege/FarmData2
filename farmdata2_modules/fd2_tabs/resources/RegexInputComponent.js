33fd14ebf4e7b38d3d648999700867f70401617f
FutzMonitor <cgonzalez.smdc@gmail.com>
2022-06-21 10:57:28 -0400

    The component can now watch for a valid/invalid input and change the color of the box accordingly.

--- farmdata2_modules/fd2_tabs/resources/RegexInputComponent.js
+++ farmdata2_modules/fd2_tabs/resources/RegexInputComponent.js
@@ -1,8 +1,29 @@
-
+/**
+ * A Vue component for input HTML elements that use Regex to validate user input
+ * 
+ * <p><b>data-cy attributes</b></p>
+ * <table>
+ * <thead><tr><th>Value</th>        <th>Descripion</th></tr></thead>
+ * <tbody>
+ * <tr><td>dropdown-component</td>  <td>div containing the label and select elements.</td></tr>
+ * <tr><td>dropdown-input</td>      <td>The select element.</td></tr>
+ * <tr><td>optioni</td>             <td>The ith option element, i=0,1,2...</td></tr>
+ * </tbody>
+ * </table>
+ * 
+ * @vue-prop {RegExp} [regExp] - The regular expression that will be used to valid user input.
+ * @vue-prop {String} [defaultType] - Defines the HTML input type. Set to 'text' by default.
+ * @vue-prop {String} [defaultColor] - Defines the background color of the input element.
+ * @vue-prop {String} [defaultHeight] - Defines the height of the input element.
+ * @vue-prop {String} [defaultWidth] - Defines the width of the input element.
+ * @vue-prop {String} [defaultVal] - Defines the value inside the input element. Set to null by default.
+ * 
+ * @vue-event {Boolean} update-color - Emits the Boolean of the last input and updates the background color depending on whether a valid or invalid was computed. 
+ */ 
 let RegexInputComponent = {
   template: 
       `<span>
-      <input v-model='val' :style='inputStyle' :type='defineType' @blur='blurEventHandler($event)'>
+      <input v-model='val' :style='inputStyle' :type='defaultType' @blur='blurEventHandler($event)'>
        </span>`, 
 
        props: {
@@ -11,27 +32,22 @@ let RegexInputComponent = {
             default: null
           },
 
-          match:{
-            type: Boolean,
-            default: null
-          },
-
-          defineType: {
+          defaultType: {
             type: String,
             default: 'text'
           },
 
-          defineBColor: {
+          defaultColor: {
             type: String,
             default: null
           },
 
-          defineHeight: {
+          defaultHeight: {
             type: String,
             default: '25px'
           },
 
-          defineWidth: {
+          defaultWidth: {
             type: String,
             default: '4em'
           },
@@ -43,14 +59,14 @@ let RegexInputComponent = {
         data () {
           return {
             val: this.defaultVal,
-            selectType : this.defineType,
+            selectType : this.defaultType,
+            match: false,
 
             inputStyle: {
-              backgroundColor: this.defineBColor,
-              height: this.defineHeight,
-              width: this.defineWidth,
-          },
-
+              backgroundColor: this.defaultColor,
+              height: this.defaultHeight,
+              width: this.defaultWidth,
+            },
           }
         },
         methods: {
@@ -66,14 +82,32 @@ let RegexInputComponent = {
           const re = new RegExp(this.regExp)
           console.log(this.regExp + " this is the re var: " + re)
           console.log("We are checking the Regex: " + this.regExp + ", against this value: " + check)
-          const validation = re.test(this.check)
-          console.log("This is the result of the validation: " + validation)
+          this.match = re.test(check)
+          console.log("This is the result of the validation: " + this.match)
+          this.updateColor(this.match)
         },
+
+        updateColor(valid){
+          console.log('HEY! HELLO! LISTEN!')
+          this.$emit('update-color', valid)
+        }
+
+
       },
       watch: {
-          
+        defaultVal(newVal) {
+          this.val = newVal
+        },
+        defaultColor(newColor) {
+          var s = new Option().style;
+          s.color = newColor;
+          this.default = newColor;
+          this.inputStyle.backgroundColor = newColor;
+        
       }
     }
+    
+  }
 
 /*
 * Export the RegexInputComponent object as a CommonJS component
