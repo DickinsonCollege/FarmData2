62137c34aa09f19b847fb56fa7369c2917e9c3ff
FutzMonitor <cgonzalez.smdc@gmail.com>
2022-07-07 09:23:56 -0400

    Updated UI Page & Its Testing Page 1. Added conditional statements to the catch(err) to handle the three types of possible errors per Axios documentation. 2. Left documentation from that page so that the different conditionals explain what type of errors they are catching. 3.Updated the UI testing page to include three tests for each type of error the API standard error handler is supposed to catch

--- farmdata2_modules/fd2_tabs/fd2_example/ui.spec.js
+++ farmdata2_modules/fd2_tabs/fd2_example/ui.spec.js
@@ -305,16 +305,20 @@ describe('Test the UI component', () => {
         })
     })
 
-    context.only('check the api error handler example', () => {
+    context('check the api error handler example', () => {
 
-        it('spinner appears while loading', () => {
+        it('forefully trigger an error code not within 2xx', () => {
             cy.get('[data-cy=alert-err-handler]')
                 .should('not.visible')
             cy.get('[data-cy=loading-err-spinner]')
                 .should('not.exist')
-
+                
+            cy.intercept('GET', '/farm/fd2-example/abc',{ statusCode: 500 })
+                .as('getServerFailure')
+            
             cy.get('[data-cy=fetch-err-api]')
                 .click()
+            cy.wait('@getServerFailure')
 
             cy.get('[data-cy=alert-err-handler]')
                 .should('be.visible')
@@ -332,6 +336,72 @@ describe('Test the UI component', () => {
             cy.get('[data-cy=loading-err-spinner]')
                 .should('not.exist')
 
+            cy.get('[data-cy=alert-err-handler]')
+                .should('be.visible')
+                .click()
+                .should('not.visible')
+        })
+
+        it('forefully trigger a network failure error', () => {
+            cy.get('[data-cy=alert-err-handler]')
+                .should('not.visible')
+            cy.get('[data-cy=loading-err-spinner]')
+                .should('not.exist')
+            
+            cy.intercept('GET', '/farm/fd2-example/abc',{ forceNetworkError: true })
+                .as('getNetworkFailure')
+            
+            cy.get('[data-cy=fetch-err-api]')
+                .click()
+            cy.wait('@getNetworkFailure')
+        
+            cy.get('[data-cy=alert-err-handler]')
+                .should('be.visible')
+            cy.get('[data-cy=first-log-name]')
+                .should('have.text','')
+            cy.get('[data-cy=loading-err-spinner]')
+                .should('not.exist')
+            cy.get('[data-cy=last-log-name]')
+                .should('have.text','')
+            cy.get('[data-cy=loading-err-spinner]')
+                .should('not.exist')
+            cy.get('[data-cy=last-log-name]')
+                .should('have.text','')
+        
+            cy.get('[data-cy=loading-err-spinner]')
+                .should('not.exist')
+    
+            cy.get('[data-cy=alert-err-handler]')
+                .should('be.visible')
+                .click()
+                .should('not.visible')
+        })
+
+        it('something happened while setting up the request that triggered an error', () => {
+            cy.get('[data-cy=alert-err-handler]')
+                .should('not.visible')
+            cy.get('[data-cy=loading-err-spinner]')
+                .should('not.exist')
+            
+            cy.get('[data-cy=fetch-err-api]')
+                .click()
+        
+            cy.get('[data-cy=alert-err-handler]')
+                .should('be.visible')
+            cy.get('[data-cy=first-log-name]')
+                .should('have.text','')
+            cy.get('[data-cy=loading-err-spinner]')
+                .should('not.exist')
+            cy.get('[data-cy=last-log-name]')
+                .should('have.text','')
+            cy.get('[data-cy=loading-err-spinner]')
+                .should('not.exist')
+            cy.get('[data-cy=last-log-name]')
+                .should('have.text','')
+        
+            cy.get('[data-cy=loading-err-spinner]')
+                .should('not.exist')
+    
             cy.get('[data-cy=alert-err-handler]')
                 .should('be.visible')
                 .click()
