140662bfdb05f54740a8fbc3da924a260bd92bc1
Chris <31524934+FutzMonitor@users.noreply.github.com>
2022-07-11 16:29:31 -0400

    Implementing Standard Error Handling for API Request Failures (#524)
    * Added API Error Handling in UI Page
    1. Fixed a small typo in the Seeding Input form
    2. Added an example of error handling inside of the UI page
    3. Added a test in the UI's e2e to cover this new example
    
    * Updated UI Page & Its Testing Page
    1. Added conditional statements to the catch(err) to handle the three types of possible errors per Axios documentation.
    2. Left documentation from that page so that the different conditionals explain what type of errors they are catching.
    3.Updated the UI testing page to include three tests for each type of error the API standard error handler is supposed to catch
    
    * Updated the Seeding Input Page & Cypress e2e Test
    1. Added the error handling to the API calls inside of the Seeding Input Page.
    2. Added the alert banner to display inside of the Seeding Input page when an API call fails.
    3. Added Cypress tests into the Seeding Inputs e2e test file to cover API fail cases.
    4. Changes some small things in the UI page with an array being shared between two tests. (They still do, but it now clears itself when the other's button is clicked)
    5. Made some small changes to the UI Cypress e2e page.
    
    * Updated the Seeding Report Page and Cypress e2e Test
    1. Added the API failure banner to the Seeding Report and all the catch errors for API request in the page.
    2. Added API failure tests to Cypress e2e test file for the Seeding Report
    3. Made some small changes to the Seeding Input e2e test file.
    
    * Fixed Indentation and Removed Unnecessary Documentation
    
    * Updated the Seeding Input & Report Pages and Test for Auto Scroll
    1. Added a scroll to feature that scrolls to the top of the page for the error banner in both the Seeding Input and Seeding Report.
    2. Added tests for this feature into both e2e Cypress tests
    
    * Simplied Catch Statement in All API Calls
    1. Removed the conditionals to catch different types of errors.
    
    * Removed Some more Redundant Code
    1. Removed the intricate conditonals in the Seeding Input for a more simple approach
    
    * Changed All the scrollTo's to scrollBy's
    
    * Changed the Auto Scroll to be More in Line with Seeding Input PR
    1. Changed the auto scroll for the error banner to be in-line with the auto scroll for the success and cancel banners inside of the Seeding Input
    2. Moved the error banner inside of the same div as the other banners inside of the Seeding Input.
    3. Used the same div format inside of the Seeding Report so that things remain consistent throughout different pages.
    4. Made some accomodations to the tests for these recent changes.

--- farmdata2_modules/fd2_tabs/fd2_example/ui.spec.js
+++ farmdata2_modules/fd2_tabs/fd2_example/ui.spec.js
@@ -304,4 +304,108 @@ describe('Test the UI component', () => {
                 .should('not.exist')
         })
     })
+
+    context('check the api error handler example', () => {
+
+        it('forefully trigger an error code not within 2xx', () => {
+            cy.get('[data-cy=alert-err-handler]')
+                .should('not.visible')
+            cy.get('[data-cy=loading-err-spinner]')
+                .should('not.exist')
+                
+            cy.intercept('GET', '/farm/fd2-example/abc',{ statusCode: 500 })
+                .as('getServerFailure')
+            
+            cy.get('[data-cy=fetch-err-api]')
+                .click()
+            cy.wait('@getServerFailure')
+
+            cy.get('[data-cy=alert-err-handler]')
+                .should('be.visible')
+            cy.get('[data-cy=first-log-name]')
+                .should('have.text','')
+            cy.get('[data-cy=loading-err-spinner]')
+                .should('not.exist')
+            cy.get('[data-cy=last-log-name]')
+                .should('have.text','')
+            cy.get('[data-cy=loading-err-spinner]')
+                .should('not.exist')
+            cy.get('[data-cy=last-log-name]')
+                .should('have.text','')
+        
+            cy.get('[data-cy=loading-err-spinner]')
+                .should('not.exist')
+
+            cy.get('[data-cy=alert-err-handler]')
+                .should('be.visible')
+                .click()
+                .should('not.visible')
+        })
+
+        it('forefully trigger a network failure error', () => {
+            cy.get('[data-cy=alert-err-handler]')
+                .should('not.visible')
+            cy.get('[data-cy=loading-err-spinner]')
+                .should('not.exist')
+            
+            cy.intercept('GET', '/farm/fd2-example/abc', { forceNetworkError: true })
+                .as('getNetworkFailure')
+            
+            cy.get('[data-cy=fetch-err-api]')
+                .click()
+            cy.wait('@getNetworkFailure')
+        
+            cy.get('[data-cy=alert-err-handler]')
+                .should('be.visible')
+            cy.get('[data-cy=first-log-name]')
+                .should('have.text','')
+            cy.get('[data-cy=loading-err-spinner]')
+                .should('not.exist')
+            cy.get('[data-cy=last-log-name]')
+                .should('have.text','')
+            cy.get('[data-cy=loading-err-spinner]')
+                .should('not.exist')
+            cy.get('[data-cy=last-log-name]')
+                .should('have.text','')
+        
+            cy.get('[data-cy=loading-err-spinner]')
+                .should('not.exist')
+    
+            cy.get('[data-cy=alert-err-handler]')
+                .should('be.visible')
+                .click()
+                .should('not.visible')
+        })
+
+        it('something happened while setting up the request that triggered an error', () => {
+            cy.get('[data-cy=alert-err-handler]')
+                .should('not.visible')
+            cy.get('[data-cy=loading-err-spinner]')
+                .should('not.exist')
+            
+            cy.get('[data-cy=fetch-err-api]')
+                .click()
+        
+            cy.get('[data-cy=alert-err-handler]')
+                .should('be.visible')
+            cy.get('[data-cy=first-log-name]')
+                .should('have.text','')
+            cy.get('[data-cy=loading-err-spinner]')
+                .should('not.exist')
+            cy.get('[data-cy=last-log-name]')
+                .should('have.text','')
+            cy.get('[data-cy=loading-err-spinner]')
+                .should('not.exist')
+            cy.get('[data-cy=last-log-name]')
+                .should('have.text','')
+        
+            cy.get('[data-cy=loading-err-spinner]')
+                .should('not.exist')
+    
+            cy.get('[data-cy=alert-err-handler]')
+                .should('be.visible')
+                .click()
+                .should('not.visible')
+        })
+    })
 })
