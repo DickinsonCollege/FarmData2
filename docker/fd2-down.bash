a4aea44ec67e2fc54a3332ad1d40e094ba2d4ff4
braughtg <braught@dickinson.edu>
2022-07-26 16:10:08 -0400

    Rewrote up and down scritps

--- docker/fd2-down.bash
+++ docker/fd2-down.bash
@@ -1,2 +1,13 @@
 #!/bin/bash
-docker-compose down
+# Take all of the containers down
+#   The --volumes delete the volumecreted by the myPHPAdmin container.
+
+echo "Stopping FarmData2..."
+docker-compose stop
+
+echo "Deleting containers..."
+docker rm fd2_mariadb
+docker rm fd2_phpmyadmin --volumes
+docker rm fd2_farmdata2
+
+echo "Done."
--- docker/fd2-up.bash
+++ docker/fd2-up.bash
@@ -1,8 +1,22 @@
 #!/bin/bash
-docker rm $(docker ps -a -q -f name=fd2_) > /dev/null 2>&1
+#docker rm $(docker ps -a -q -f name=fd2_) > /dev/null 2>&1
 
-docker-compose up -d
+# Delete any of the existing containers (except dev)
+echo "Starting FarmData2..."
+
+echo "Removing any stale containers..."
+docker rm fd2_mariadb &> /dev/null
+docker rm fd2_phpmyadmin &> /dev/null
+docker rm fd2_farmdata2 &> /dev/null
+
+echo "Starting containers..."
+# Note: Any command line args are passed to the docker-compose up command
+# Useful for: --force-recreate in particular.
+docker-compose up -d "$@"
 
 sleep 3  # give site time to come up before clearing the cache.
 
+echo "Clearing drupal cache..."
 docker exec -it fd2_farmdata2 drush cc all
+
+echo "Done."
