c0d89b522fbd06054715ac7949ba7655f5a3529e
JingyuMarcelLee <leemar@dickinson.edu>
2022-07-26 15:13:28 -0400

    completed component and tests

--- farmdata2_modules/fd2_tabs/resources/ErrorBannerComponent.js
+++ farmdata2_modules/fd2_tabs/resources/ErrorBannerComponent.js
@@ -1,22 +1,48 @@
 let ErrorBannerComponent = {
     template: 
-    `<span>
-        <div data-cy='alert-err-handler' class="alert alert-danger" style="display: none;" role="alert" v-show="errBanner == true" @click="hideBanner">
-            <p v-model="errMessage"></p>
-        </div>
-    </span>`,
+        `<div data-cy='alert-err-handler' class="alert alert-danger" id="banner" role="alert" v-show="isVisible" @click="hideBanner">
+            <p data-cy='alert-err-message'>{{ message }}</p>
+        </div>`,
     props: {
-        default: {
-
+        errMessage: {
+            type: String,
+            required: true,
+        },
+        visible: {
+            type: Boolean,
+            required: true,
+            default: false,
         }
     },
     data() {
         return {
-
+            message: this.errMessage,
+            isVisible: this.visible,
         }
     },
     watch: {
-
+        visible(newbool) {
+            this.isVisible = newbool;
+            this.ScrollToBanner()
+        },
+        errMessage(newString) {
+            this.message = newString;
+        }
+    },
+    methods: {
+        hideBanner() {
+            this.isVisible = false
+        },
+        ScrollToBanner() {
+            var errorBanner = document.getElementById('banner')
+            var pageHeader = document.getElementById('navbar')
+            var headerBounds = pageHeader.getBoundingClientRect()
+            var alertBounds = errorBanner.getBoundingClientRect()
+            scrollBy({
+                top: alertBounds.top - headerBounds.bottom,
+                behavior: 'smooth'
+            })
+        }
     }
 }
 /*
