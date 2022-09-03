d62a82b909e5c0e4ef205ac1868598e8cc9119ee
braughtg <braught@dickinson.edu>
2022-09-03 16:56:20 -0400

    Using fd2 specific images for mariaDB and phpmyadmin

---
+++ docker/build-images.bash
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Build and push to DockerHub multi architecture images
+# for all of the containers used by FarmData2.
+
+LOGGED_IN=$(cat ~/.docker/config.json | grep "index.docker.io" | wc -l | cut -f 8 -d ' ')
+if [ "$LOGGED_IN" == "0" ];
+then
+  echo "Please log into Docker Hub before building images."
+  echo "  Use: docker login"
+  echo "This allows multi architecture images to be pushed."
+  exit -1
+fi
+
+if [ $# -lt 1 ];
+then
+  echo "Please specify one or more images to build."
+  echo "E.g. build-images.bash dev farmOS"
+  echo "  Valid images to build are the directories in the docker folder"
+  echo "  that contain a Dockerfile."
+  exit -1
+fi
+
+# Create the builder if it doesn't exist.
+FD2_BUILDER=$(docker buildx ls | grep "fd2builder" | wc -l | cut -f 8 -d ' ')
+if [ "$FD2_BUILDER" == "0" ];
+then
+  echo "Making new builder for FarmData2 images."
+  #docker buildx create --name fd2builder
+fi
+
+# Switch to use the fd2builder.
+echo "Using the fd2bilder."
+#docker buildx use fd2builder
+
+# Build and push each of the images to Docker Hub.
+for IMAGE in "$@"
+do
+  if [ ! -f $IMAGE/Dockerfile ] | [ ! -f $IMAGE/repo.txt ];
+  then
+    echo "Error: $IMAGE/Dockerfile or $IMAGE/repo.txt does not exit."
+    echo "       Skipping $IMAGE"
+  else
+    echo "Building $IMAGE..."
+
+    cd $IMAGE
+    TAG=$(cat repo.txt)
+    REPO="farmdata2/$TAG"
+    docker buildx build --platform linux/amd64,linux/arm64 -t $REPO --push .
+    cd ..
+
+    echo "Done building $IMAGE."
+  fi
+done
