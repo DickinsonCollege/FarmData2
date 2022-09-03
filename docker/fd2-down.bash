3ff0af03e98eb3bff61f84b2d58ad74e963d869f
braughtg <braught@dickinson.edu>
2022-09-03 14:49:45 -0400

    Clarified --volumes flag

--- docker/fd2-down.bash
+++ docker/fd2-down.bash
@@ -1,6 +1,5 @@
 #!/bin/bash
 # Take all of the containers down
-#   The --volumes delete the volumecreted by the myPHPAdmin container.
 
 echo "Stopping Containers..."
 docker stop fd2_phpmyadmin
@@ -9,7 +8,7 @@ docker stop fd2_mariadb
 
 echo "Deleting containers..."
 docker rm fd2_mariadb
-docker rm fd2_phpmyadmin --volumes
+docker rm fd2_phpmyadmin --volumes  # remove /sessions volume created.
 docker rm fd2_farmdata2
 
 echo "Stopping dev container..."
