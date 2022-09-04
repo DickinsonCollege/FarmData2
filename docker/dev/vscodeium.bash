abc87fd6b02e27013532ed626a332e99e18eafff
braughtg <braught@dickinson.edu>
2022-09-04 08:35:16 -0400

    Switch to VSCodium from VSCode

---
+++ docker/dev/vscodeium.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Note: VS Codium is an open source licenced version of VS code
+# that has all of the MS telemetry stripped out of it.
+# Better for privacy and seems more stable running in docker than VS Code.
+
+# Approach from:
+# https://vscodium.com/#install
+
+wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg \
+    | gpg --dearmor \
+    | sudo dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg
+
+echo 'deb [ signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg ] https://download.vscodium.com/debs vscodium main' \
+    | sudo tee /etc/apt/sources.list.d/vscodium.list
+
+apt update
+apt install -y codium
+
+# Patch the xfce menu item for VS Code so it runs correctly.
+sed -i 's+usr/share/codeium/code+/usr/bin/codeium --no-sandbox+g' /usr/share/applications/codeium.desktop
