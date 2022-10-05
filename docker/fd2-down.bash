22d54e3505d43c3e7c28acae21ce3615a2978f4b
braughtg <braught@dickinson.edu>
2022-10-05 17:10:11 -0400

    Explicitly stop fd2_dev container

--- docker/fd2-down.bash
+++ docker/fd2-down.bash
@@ -5,12 +5,11 @@ echo "Stopping Containers..."
 docker stop fd2_phpmyadmin
 docker stop fd2_farmdata2
 docker stop fd2_mariadb
+docker stop fd2_dev
 
 echo "Deleting containers..."
 docker rm fd2_mariadb
 docker rm fd2_phpmyadmin --volumes  # remove /sessions volume created.
 docker rm fd2_farmdata2
 
-echo "Stopping dev container..."
-docker compose --profile windows,unix stop
 echo "Done."
