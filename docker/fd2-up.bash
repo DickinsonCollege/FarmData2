3a4e593400963745b6adf6b36044b5a5a1b8e9e1
braughtg <braught@dickinson.edu>
2022-10-16 14:17:40 -0400

    Added check for root

--- docker/fd2-up.bash
+++ docker/fd2-up.bash
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Ensuring this script is not being run as root.
+RUNNING_AS_ROOT=$(id -un | grep "root" )
+if [ -n "$RUNNING_AS_ROOT" ];
+then
+  echo "The fd2-up.bash script should not be run as root."
+  echo "Please run without using sudo."
+  exit -1
+fi
+
 echo "Starting FarmData2..."
 
 # Get the name of the directory containing the FarmData2 repo.
@@ -77,91 +86,94 @@ FD2_GID=23433
 
 if [ "$PROFILE" == "windows" ] || [ "$PROFILE" == "linux" ] ;
 then
-  echo "Configuring windows (WSL) or Linux host..."
+  echo "Configuring Windows (WSL 2) or Linux host..."
 
   # If the docker group doesn't exist, create it.
   DOCKER_GRP_EXISTS=$(grep "docker" /etc/group)
   if [ -z "$DOCKER_GRP_EXISTS" ];
   then
-    echo "  Docker group does not exits on host."
+    echo "  Docker group does not exit on host."
+  else 
+    echo "  Docker group exists on host."
   fi
 
   # If the current user is not in the docker group add them.
   USER_IN_DOCKER_GRP=$(groups | grep "docker")
   if [ -z "$USER_IN_DOCKER_GRP" ];
   then 
-    echo "  Current user $(id -un) is not in the docker group."
+    echo "  User $(id -un) is not in docker group."
+  else
+    echo "  User $(id -un) is in docker group."
   fi
 
   # If the /var/run/docker.sock is not in the docker group add it.
   SOCK_IN_DOCKER_GRP=$(ls -l /var/run/docker.sock | grep " docker ")
   if [ -z "$SOCK_IN_DOCKER_GRP" ];
   then
-    echo "  The /var/run/docker.sock file is not in the docker group."
+    echo "  /var/run/docker.sock is not in docker group."
+  else
+    echo "  /var/run/docker.sock is in docker group."
   fi
 
   # If the docker group does not have write permission to docker.sock add it.
   DOCKER_GRP_RW_SOCK=$(ls -l /var/run/docker.sock | cut -c 5-6 | grep "rw")
   if [ -z "$DOCKER_GRP_RW_SOCK" ];
   then
-    echo "  The docker group does not have RW access to /var/run/docker.sock."
+    echo "  docker group does not have RW access to /var/run/docker.sock."
+  else 
+    echo "  docker group has RW access to /var/run/docker.sock."
   fi
 fi
 
-exit -1
-
+echo "Configuring FarmData2 group (fd2grp)..."
 # If group fd2grp does not exist on host create it
 FD2GRP_EXISTS=$(grep "fd2grp" /etc/group)
-if ! "$FD2GRP_EXISTS" ;
+if [ -z "$FD2GRP_EXISTS" ];
 then
+  echo "  fd2grp group does not exist on host."
+else
+  echo "  fd2grp group exists on host."
 fi
 
 # If the current user is not in the fd2grp then add them.
-IN_FD2GRP=$(groups | grep "fd2grp")
-if ! "$IN_FD2GRP" ;
+USER_IN_FD2GRP=$(groups | grep "fd2grp")
+if [ -z "$USER_IN_FD2GRP" ];
 then
+  echo "  User $(id -un) is not in fd2grp group."
+else
+  echo "  User $(id -un) is in fd2grp group."
 fi
 
-# If the FarmData2 directory is not in the gd2grp then set it.
+# If the FarmData2 directory is not in the fd2grp then set it.
 FD2GRP_OWNS_FD2=$(ls -ld ../../$FD2_DIR | grep " fd2grp ")
-if ! $FD2GRP_OWNS_FD2 ;
+if [ -z "$FD2GRP_OWNS_FD2" ];
 then
+  echo "  $FD2_DIR is not in fd2grp group."
+else
+  echo "  $FD2_DIR is in fd2grp group."
 fi
+
+# If the fd2grp does not have RW access to FarmData2 change it.
+FD2GRP_RW_FD2=$(ls -ld ../../$FD2_DIR | cut -c 5-6 | grep "rw")
+if [ -z "$FD2GRP_RW_FD2" ];
+then
+  echo "  fd2grp does not have RW access to $FD2_DIR."
+else
+  echo "  fd2grp has RW access to $FD2_DIR."
+fi
+
+exit -1
 
 # Put GID's of docker and fd2grp groups into files in ~/.fd2 on host
 # These will be used by the startup.bash script in the dev container
 # to ensure that the fd2dev user in the container has permissions to
 # RW the FarmData2 files and /var/run/docker.sock.
 
-exit -1
+
 
 
 
 if [ "$PROFILE" == "windows" ] 
-
-  # Linux uses the host UID/GID values from mounted directories in the container.
-  # So we need to ensure that the fd2user user in the container can RW the FarmData2 direcotry.
-  # We do so by creating a group (fd2grp, GID=23432) on the host and in the container.
-  # Note that, if on the host a group with GID=23432 already exists we bail :(.
-  # Otherwise we create the group on the host. We then give that group RW access to 
-  # everything in the FarmData2 directory. The user (fd2dev) in the container is also 
-  # in the group (fd2grp, GID=23432) so then it may RW all of the mounted FarmData2 direcotry.
-  IN_FD2_GRP=$(groups | grep "fd2grp" | wc -l)
-  if [ "$IN_FD2_GRP" == "0" ]
-  then
-    # Current user is no in the fd2grp so we need to do all of this...
-    # otherwise it has all already been done.
-    GRP_EXISTS=$(grep ":23432:" /etc/group)
-    if [ "$GRP_EXISTS" != "" ]
-    then
-      echo "Your Linux host has an existing group with GID 23432."
-      echo "FarmData2 is trying to use this GID."
-      echo "This is an unlikekly situation that we have not yet dealt with."
-      echo "If you encountere this message, please contact us."
-      echo "We will work to resolve it."
-      exit -1
-    fi
-
     echo "FarmDat2 needs to create a new group fd2grp on your machine."
     echo "Your user "$(whoami)" will be added to the group fd2grp."
     echo "The FarmData2 directory will be added to the group fd2grp."
@@ -183,7 +195,6 @@ if [ "$PROFILE" == "windows" ]
     newgrp fd2grp
 
     PROFILE=unix
-  fi
 fi
 
 
