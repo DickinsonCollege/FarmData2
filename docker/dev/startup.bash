b471aee56c45686e096b0a12b8ac2bf1cab08db1
braughtg <braught@dickinson.edu>
2022-10-16 20:55:33 -0400

    Fixed groupadd gid flag

--- docker/dev/startup.bash
+++ docker/dev/startup.bash
@@ -8,7 +8,7 @@ rm /tmp/.X1-lock
 # Create the docker group and ensure that the docker.sock 
 # has the appropriate permissions.
 echo "fd2dev" | sudo -S groupdel docker 
-echo "fd2dev" | sudo -S groupadd --GID $(cat /.fd2/docker.gid) docker
+echo "fd2dev" | sudo -S groupadd --gid $(cat /.fd2/docker.gid) docker
 echo "fd2dev" | sudo -S usermod -a -G docker fd2dev
 echo "fd2dev" | sudo -S chgrp docker /var/run/docker.sock
 echo "fd2dev" | sudo -S chmod 775 /var/run/docker.sock
@@ -17,7 +17,7 @@ echo "fd2dev" | sudo -S chmod 775 /var/run/docker.sock
 # FarmData2 and fd2test files and directories all have the 
 # correct permissions.
 echo "fd2dev" | sudo -S groupdel fd2grp
-echo "fd2dev" | sudo -S groupadd --GID $(cat /.fd2/fd2grp.gid) fd2grp
+echo "fd2dev" | sudo -S groupadd --gid $(cat /.fd2/fd2grp.gid) fd2grp
 echo "fd2dev" | sudo -S usermod -a -G fd2grp fd2dev
 echo "fd2dev" | sudo -S chgrp -R fd2grp /home/fd2dev/FarmData2
 echo "fd2dev" | sudo -S chmod -R g+rw /home/fd2dev/FarmData2
