63ea0a1bf1a0f06f760489343ae9dea5c160beca
braughtg <braught@dickinson.edu>
2022-10-18 15:08:37 -0400

    Ensure fd2-down is run from host

--- docker/fd2-down.bash
+++ docker/fd2-down.bash
@@ -1,6 +1,15 @@
 #!/bin/bash
 # Take all of the containers down
 
+# Ensure that this script is not being run in the development container.
+HOST=$(docker inspect -f '{{.Name}}' $HOSTNAME 2> /dev/null)
+if [ "$HOST" == "/fd2_dev" ];
+then
+  echo "The fd2-down.bash script should not be run in the dev container."
+  echo "Always run fd2-down.bash on your host OS."
+  exit -1
+fi
+
 echo "Stopping Containers..."
 docker stop fd2_phpmyadmin
 docker stop fd2_farmdata2
