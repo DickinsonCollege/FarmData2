2f9b5d35011e9feac18916e860ae536fd118647b
braught <braught@dickinson.edu>
2022-12-20 11:43:41 -0500

    Added waitForPage command

--- farmdata2_modules/cypress/support/commands.js
+++ farmdata2_modules/cypress/support/commands.js
@@ -1,25 +1,54 @@
 Cypress.Commands.add("login", (user, password) => {
-    cy.request({
-        method: 'POST',
-        url: '/user/login',
-        form: true,
-        body: {
-            name: user,
-            pass: password,
-            form_id: 'user_login'
-        }
-    })
+  cy.request({
+      method: 'POST',
+      url: '/user/login',
+      form: true,
+      body: {
+          name: user,
+          pass: password,
+          form_id: 'user_login'
+      }
+  })
 });
 
 Cypress.Commands.add("logout", () => {
-    cy.request({
-        method: 'POST',
-        url: '/user/logout',
-        form: true,
-        body: {
-        }
-    })
+  cy.request({
+      method: 'POST',
+      url: '/user/logout',
+      form: true,
+      body: {
+      }
+  })
 });
+
+/**
+ * This command works with a pattern in the page to wait for 
+ * api calls initiated in the created() method to complete.
+ * 
+ * To use this command the page should contain the following:
+ * 
+ * The <div>:
+ *   <div data-cy="page-loaded" v-show=false>{{ pageLoaded }}</div>
+ * 
+ * The data element:
+ *   createdCount: 0,
+ * 
+ * A computed function where the value in the comparison (e.g. 1) 
+ * is determined by the number of api calls to wait for:
+ *   pageLoaded() {
+ *     return this.createdCount == 1
+ *   },
+ * 
+ * In the created() method, in a then() associated with each API call
+ * include the line:
+ *   this.createdCount++
+ * 
+ * See the fd2_example/vars/vars.html and fd2_example/api/api.html pages
+ * for some examples.
+ */
+Cypress.Commands.add("waitForPage", () => {
+  cy.get('[data-cy=page-loaded]').should('have.text','true')
+})
 
 /**
  * Cypress clears the local storage between tests.  This work around
