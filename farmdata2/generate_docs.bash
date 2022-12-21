737c8b1b5d16048bd527c9c87a22bcbf72574c42
braught <braught@dickinson.edu>
2022-12-21 16:41:22 -0500

    Modifing doc generation

--- farmdata2/generate_docs.bash
+++ farmdata2/generate_docs.bash
@@ -10,7 +10,24 @@ then
     exit -1
 fi
 
-rm -rf doc
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+echo "Generating docs in $SCRIPT_DIR..."
 
-jsdoc ./farmdata2_modules/resources -r -c ./jsdoc/JSDoc.json -d doc
-jsdoc ./farmdata2_api/src -r -c ./jsdoc/JSDoc.json -d doc
+echo "  Deleting old docs."
+rm -rf $SCRIPT_DIR/doc
+
+JSCONF=$SCRITP_DIR/jsdoc/JSDoc.json
+echo "  Using JSDoc configuration from $JSCONF."
+
+DOC_DIR=$SCRIPT_DIR/doc
+echo "  Sending output to $DOC_DIR."
+
+RESOURCES=$SCRIPT_DIR/farmdata2_modules/resources
+echo "  Generating docs for $RESOURCES."
+jsdoc $RESOURCES -r -c $JSCONF -d $DOC_DIR
+
+API=$SCRIPT_DIR/farmdata2_api/src
+echo "  Generating docs for $RESOURCES."
+jsdoc $API -r -c $JSCONF -d $DOC_DIR
+
+echo "Done."
