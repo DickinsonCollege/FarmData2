3099653c5aa8705582ef94d936317b8ac219de2a
braught <braught@dickinson.edu>
2022-12-21 10:07:44 -0500

    Updated comments and messages

--- farmdata2_modules/fd2_tabs/fd2_example/vars/vars.config.spec.js
+++ farmdata2_modules/fd2_tabs/fd2_example/vars/vars.config.spec.js
@@ -1,8 +1,8 @@
 /**
  * The vars.html page loads and displays the contents of 
  * configuration saved by the fd2_config module.  This spec
- * verifies that the config is loaded and displayed in the
- * vars page.
+ * verifies that the settings in the config are displayed in 
+ * the vars page.
  */
 
 var FarmOSAPI = require('../../resources/FarmOSAPI.js')
@@ -11,7 +11,7 @@ var getSessionToken = FarmOSAPI.getSessionToken
 var getConfiguration = FarmOSAPI.getConfiguration
 var setConfiguration = FarmOSAPI.setConfiguration
 
-describe('Test the values saved by the config module.', () => {
+describe('Test that the page displays the values saved by the config module.', () => {
 
     let sessionToken = null
     let configMap = null
@@ -19,10 +19,14 @@ describe('Test the values saved by the config module.', () => {
     beforeEach(() => {
         cy.login('worker1', 'farmdata2')
         .then(() => {
-            // Using wrap to wait for the maps to load.
+            // Get the session token so that the config can be changed by the tests.
             cy.wrap(getSessionToken()).as('token')
+            // Get the current configuration so that the display can be checked.
             cy.wrap(getConfiguration()).as('getConfigMap')
         })
+
+        // Wait for the API calls that get the session token and configuration to 
+        // return before continuing with the tests.
         cy.get('@token').then(function(token) {
             sessionToken = token
         })
@@ -31,62 +35,22 @@ describe('Test the values saved by the config module.', () => {
         })
         
         cy.visit('/farm/fd2-example/vars')
+        // The Var's page uses the configuration object so we 
+        // wait here to be sure the page has that information
+        // before continuing.
         cy.waitForPage()
     })
 
-    it("check values in the table when the page is loaded", () => {
+    it("Check the configuration values in the table when the page is loaded.", () => {
         let keys = Object.keys(configMap)
-        // for loop to check the headings and data in the table
+
+        // Loop through the map we loaded in the test and compare each
+        // key and value to those displayed by the page.
         for (let i = 0; i < keys.length; i++) {
             cy.get('[data-cy='+i+'configName]')
                 .should('have.text', keys[i])
             cy.get('[data-cy='+i+'data]')
                 .should('have.text', configMap[keys[i]])
         } 
-    })
-
-    it("check values in the table after the values are updated", () => {
-        // check initial values 
-        let keys = Object.keys(configMap)
-        
-        for (let i = 0; i < keys.length; i++) {
-            cy.get('[data-cy='+i+'configName]')
-                .should('have.text', keys[i])
-            cy.get('[data-cy='+i+'data]')
-                .should('have.text', configMap[keys[i]])
-        } 
-        
-        // Change the config object
-        let newConfigObj = { id:1, labor: "Optional"}
-        cy.intercept('GET', '/fd2_config/1').as('newconfig')
-        cy.wrap(setConfiguration(newConfigObj, sessionToken)).as(`resetConfig`)
-
-        // read the config map again to verify update
-        cy.get(`@resetConfig`)
-        .then(() => {
-            cy.wrap(getConfiguration()).as('getNewConfigMap')
-        })
-        
-        let newConfigMap = null
-        cy.get('@getNewConfigMap')
-        .then(function(map) {
-            newConfigMap = map.data
-        })
-
-        // Reload the page and check if the config was updated.
-        cy.reload()
-        .then(() => {
-            let newkeys = Object.keys(newConfigMap)
-            for (let i = 0; i < newkeys.length; i++) {
-                cy.get('[data-cy='+i+'configName]')
-                    .should('have.text', newkeys[i])
-                cy.get('[data-cy='+i+'data]')
-                    .should('have.text', newConfigMap[newkeys[i]])
-            } 
-        })
-        .then(() => {
-            // Set the config back to what it was initially.
-            setConfiguration(configMap, sessionToken)
-        })
     })
 })
--- farmdata2_modules/fd2_tabs/fd2_example/vars/vars.uid.spec.js
+++ farmdata2_modules/fd2_tabs/fd2_example/vars/vars.uid.spec.js
@@ -1,10 +1,10 @@
 /**
  * The .module file defines some JS variables that are 
- * available in each sub-tab. This spec tests that those
- * variables are available.
+ * available in each subtab. This spec tests that the 
+ * Vars subtabp are is able to display those variables.
  */
-describe('Test the fd2 javascript variables defined by the module', () => {
-    context('check vars as the admin user', () => {
+describe('Test that module can access the fd2 javascript variables defined by the module', () => {
+    context('Test when logged in as the admin user', () => {
         
         beforeEach(() => {
             cy.login('admin', 'farmdata2')
@@ -20,7 +20,7 @@ describe('Test the fd2 javascript variables defined by the module', () => {
             cy.visit('/farm/fd2-example/vars')
         })
 
-        it('check variables directly', () => {
+        it('Check the values of the variables directly.', () => {
             // Note:  It would be better to use the username to user id map here
             // so that this will work if the sample data changes.  It was not used
             // to keep this example simple.  See the maps.spec.js file for an example
@@ -29,58 +29,49 @@ describe('Test the fd2 javascript variables defined by the module', () => {
             cy.window().its('fd2UserName').should('equal','admin');
         })
 
-        it('check user id value', () => {
+        it('Check that the user id and user name are displayed.', () => {
             cy.get('[data-cy=user-id]')
                 .should('have.text','1')
-        })
-
-        it('check user name value', () => {
             cy.get('[data-cy=user-name]')
                 .should('have.text','admin')
         })
     })
 
-    context('check as manager1', () => {
+    context('Test when logged in as manager1', () => {
 
         beforeEach(() => {
             cy.login('manager1', 'farmdata2')
             cy.visit('/farm/fd2-example/vars')
         })
 
-        it('check variables directly', () => {
+        it('Check the values of the variables directly.', () => {
             cy.window().its('fd2UserID').should('equal',3);
             cy.window().its('fd2UserName').should('equal','manager1');
         })
 
-        it('check user id value', () => {
+        it('Check that the user id and user name are displayed.', () => {
             cy.get('[data-cy=user-id]')
                 .should('have.text','3')
-        })
-
-        it('check user name value', () => {
             cy.get('[data-cy=user-name]')
                 .should('have.text','manager1')
         })
     })
 
-    context('check as worker1',() => {
+    context('Test when logged in as worker1',() => {
 
         beforeEach(() => {
             cy.login('worker1', 'farmdata2')
             cy.visit('/farm/fd2-example/vars')
         })
 
-        it('check variables directly', () => {
+        it('Check the values of the variables directly.', () => {
             cy.window().its('fd2UserID').should('equal',5);
             cy.window().its('fd2UserName').should('equal','worker1');
         })
 
-        it('check user id value', () => {
+        it('Check that the user id and user name are displayed.', () => {
             cy.get('[data-cy=user-id]')
                 .should('have.text','5')
-        })
-
-        it('check user name value', () => {
             cy.get('[data-cy=user-name]')
                 .should('have.text','worker1')
         })
