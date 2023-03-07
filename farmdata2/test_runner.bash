093e9ad88f4dd8a2dab0ed024a2eced399538e4b
FutzMonitor <cgonzalez.smdc@gmail.com>
2023-03-07 11:30:38 -0500

    1. Changes to dev/Dockerfile - Added the creation of the .eslintignore file which is used to ignore the linting of javascript files. 2. Changes to test_runner.bash - Indentation fixes

--- farmdata2/test_runner.bash
+++ farmdata2/test_runner.bash
@@ -11,23 +11,23 @@ fi
 
 if [[ "$1" != "e2e" && "$1" != "ct" && "$1" != "all" ]];
 then
-  echo "Usage: test_runner.bash [<type> [<args>]]"
-  echo "  Run the Cypress test suite."
-  echo "    <type>:"
-  echo "      If no <type> is provided the Cypress GUI test runner will be opened."
-  echo "      If <type> is provided the terminal-based Cypress test runner is used:"
-  echo "        e2e - to run the end to end tests headless (*.spec.js)."
-  echo "        ct  - to run the component tests headless (*.spec.comp.js)."
-  echo "        all - to run both the end-to-end and component tests in sequence."
-  echo "    <args>:"
-  echo "       Ignored if no <type> is provided."
-  echo "       Otherwise, all remaining <args> are passed directly to cypress run."
-  echo "         Several useful <args> include:"
-  echo "           --browser <electron | firefox | chrome | chromium | ...>"
-  echo "           --spec <blob>"
-  echo "             e.g. --spec \"**/fd2_example/ui/*.spec.js\""
-  echo "             e.g. --spec \"**/fd2_field_kit/**/*.spec.js\""
-  exit -1
+    echo "Usage: test_runner.bash [<type> [<args>]]"
+    echo "  Run the Cypress test suite."
+    echo "    <type>:"
+    echo "      If no <type> is provided the Cypress GUI test runner will be opened."
+    echo "      If <type> is provided the terminal-based Cypress test runner is used:"
+    echo "        e2e - to run the end to end tests headless (*.spec.js)."
+    echo "        ct  - to run the component tests headless (*.spec.comp.js)."
+    echo "        all - to run both the end-to-end and component tests in sequence."
+    echo "    <args>:"
+    echo "       Ignored if no <type> is provided."
+    echo "       Otherwise, all remaining <args> are passed directly to cypress run."
+    echo "         Several useful <args> include:"
+    echo "           --browser <electron | firefox | chrome | chromium | ...>"
+    echo "           --spec <blob>"
+    echo "             e.g. --spec \"**/fd2_example/ui/*.spec.js\""
+    echo "             e.g. --spec \"**/fd2_field_kit/**/*.spec.js\""
+    exit -1
 fi
 
 ARGS=${@:2}
