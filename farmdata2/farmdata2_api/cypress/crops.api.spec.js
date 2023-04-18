23c281c60a064c56b55b886942a9e19e5e9a95f4
JingyuMarcelLee <leemar@dickinson.edu>
2023-04-18 00:55:12 -0400

    Added Cypress tests for APIs

---
+++ farmdata2/farmdata2_api/cypress/crops.api.spec.js
@@ -0,0 +1,24 @@
+describe('API Request Functions', () => {
+    beforeEach(() => {
+        // change base url from http://fd2_farmdata2 to http://fd2_api
+        Cypress.config('baseUrl', "http://fd2_api")
+    })
+
+    it('Test mapByName', () => {
+        cy.request('/crops/mapbyName').as('mapByName');
+        cy.get('@mapByName').then(mapByName => {
+            expect(mapByName.status).to.eq(200);
+            assert.isObject(mapByName.body, 'mapByName Response is an object')
+            cy.log(JSON.stringify(mapByName.body))
+        });
+    })
+
+    it('Test mapById', () => {
+        cy.request('/crops/mapById').as('mapById');
+        cy.get('@mapById').then(mapById => {
+            expect(mapById.status).to.eq(200);
+            assert.isObject(mapById.body, 'mapById Response is an object')
+            cy.log(JSON.stringify(mapById.body))
+        });
+    })
+})
