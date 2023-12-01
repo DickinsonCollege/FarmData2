998c8707612720967cdfbdf0b101b1495f6c8168
Ty Chermsirivatana <chermsit@dickinson.edu>
2023-12-01 07:40:09 -0500

    Implementation of Selective Containers at Launch (#684)
    __Pull Request Description__
    
    This PR should resolve #575 ~~and is currently in a draft phase~~.
    Basically this is going to add a phpmyadmin profile, then the PR
    modifies `fd2-up.bash` to include flags such as `--no-dev` and
    `--no-phpmyadmin.` ~~Will test and work before recommending a review.~~
    
    __Licensing Certification__
    
    FarmData2 is a [Free Cultural
    Work](https://freedomdefined.org/Definition) and all accepted
    contributions are licensed as described in the LICENSE.md file. This
    requires that the contributor holds the rights to do so. By submitting
    this pull request __I certify that I satisfy the terms of the [Developer
    Certificate of Origin](https://developercertificate.org/)__ for its
    contents.
    
    ---------
    
    Signed-off-by: Ty Chermsirivatana <chermsit@dickinson.edu>

--- docker/fd2-down.bash
+++ docker/fd2-down.bash
@@ -10,18 +10,13 @@ then
   exit -1
 fi
 
-echo "Stopping Containers..."
-docker stop fd2_phpmyadmin
-docker stop fd2_farmdata2
-docker stop fd2_mariadb
-docker stop fd2_dev
-docker stop fd2_api
+if [ -z "$COMPOSE_PROFILES" ]; then
+    echo "Error: COMPOSE_PROFILES doesn't have profiles stored. Please run fd2-up.bash to bring FarmData2 containers up first."
+    exit 1
+fi
 
-echo "Deleting containers..."
-docker rm fd2_mariadb
-docker rm fd2_phpmyadmin --volumes  # remove /sessions volume created.
-docker rm fd2_farmdata2
-docker rm fd2_dev
-docker rm fd2_api
+# I'm pretty sure since the COMPOSE_PROFILES utilize the environment variables, you can just run docker-compose down and that's it.
+docker-compose down
 
-echo "Done."
+#Unsets COMPOSE_PROFILES to prevent irregular edge cases. 
+unset COMPOSE_PROFILES
